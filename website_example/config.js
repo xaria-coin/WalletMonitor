48d424c54b82cb8d92c24e87237a0b3ba4274e0d
andrew kelly <andyckelly@outlook.com>
2018-09-18 19:11:36 +0100

    updates

--- website_example/config.js
+++ website_example/config.js
@@ -1,13 +1,13 @@
-var api = "http://poolhost:8117";
+var api = "http://159.69.86.230:8117";
 
-var email = "support@poolhost.com";
-var telegram = "https://t.me/YourPool";
-var discord = "https://discordapp.com/invite/YourPool";
+var email = "dev@xaria.xyz";
+var telegram = "https://t.me/xariacoin";
+var discord = "https://discord.gg/kDmqsZT/";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var blockchainExplorer = "http://explorer.xaria.xyz/?hash={id}#blockchain_block";
+var transactionExplorer = "http://explorer.xaria.xyz/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
